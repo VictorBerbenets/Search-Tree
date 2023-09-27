205c51cb79f7a5fb016103c01a56b11e855c4c77
Victor Berbenets <berbenets.vd@phystech.edu>
2023-09-27 23:55:30 +0300

    slowly finding errors

--- include/search_tree.hpp
+++ include/search_tree.hpp
@@ -22,23 +22,39 @@ class AVL_Tree final {
     
     static constexpr difference_type DIFF_HEIGHT = 2; // difference between two subtree heights
 
-    void right_turn(pointer& pt) {
+    void right_turn(pointer pt) {
 
     }
 
-    void big_right_turn(pointer& pt) {
+    void big_right_turn(pointer pt) {
 
     }
 
-    void left_turn(pointer& pt) {
+    void left_turn(pointer pt) {
+        auto& parent = pt->parent_;
+        if (parent) {
+            parent->left_ == pt ? parent->left_ : parent->right_ = pt->right_;
+        }
+
+        auto tmp     = pt->right_;
+        pt->parent_  = pt->right_;
+        pt->right_   = pt->right_->left_;
+        tmp->left_   = pt;
+        tmp->parent_ = parent;
+        pt = tmp;
+#if 0
+        auto right_left_subtree = pt->right_->left_;
+        std::swap(right
+        pt = tmp->right;
+        std::swap()
+#endif
+    }
+
+    void big_left_turn(pointer pt) {
+
+    }
     
-    }
-
-    void big_left_turn(pointer& pt) {
-
-    }
-    
-    difference_type height_difference(const_pointer& left, const_pointer& right) const noexcept {
+    difference_type height_difference(const_pointer left, const_pointer right) const noexcept {
         return (left ? left->height_ : 0) - (right ? right->height_ : 0);
     }
     
@@ -88,11 +104,20 @@ template <typename Iter>
         if (pt->left_) {
             print(pt->left_);
         }
-        std::cout << pt->key_ << std::endl;
+        std::cout << "key = " << pt->key_ << std::endl;
+        std::cout << "height = " << pt->height_ << std::endl;
         if (pt->right_) {
             print(pt->right_);
         }
     }
+
+    void increase_heights(pointer pt) {
+        while(pt) {
+            pt->height_++;
+            pt = pt->parent_;
+        }
+    }
+
     void insert(const KeyT& key) {
         if (root_node_ == nullptr) {
             root_node_ = new Node {key};
@@ -101,37 +126,52 @@ template <typename Iter>
         pointer parent {};
         auto curr_node = root_node_;
         while(curr_node) {
-            if (curr_node) { curr_node->height_++; }
+//            if (curr_node) { curr_node->height_++; }
             parent = curr_node;
             if (comp_(key, curr_node->key_)) {
                 if (!curr_node->left_) { 
                     curr_node->left_ = new Node {key, parent};
+                    increase_heights(curr_node);
                     break; 
                 }
                 curr_node = curr_node->left_;
-            } else if (!comp_(key, curr_node->key_)) {
+            } else if (key == curr_node->key_) {
+                return ;
+            } else {
                 if (!curr_node->right_) {
                     curr_node->right_ = new Node {key, parent};
+                    increase_heights(curr_node);
                     break;
                 }
                 curr_node = curr_node->right_;
-            } else { return ; }
+            }
         }
+        balance_tree(curr_node);
     }
     
     void balance_tree(pointer& pt) {
-        if ( is_disbalance( height_difference(pt->right, pt->left) ) ) {
-            if (height_difference(pt->right->right, pt->right->left) >= 0) {
-                left_turn(pt);
-            } else {
-                big_left_turn(pt);
+        while(pt) {
+            std::cout << "TREE\n";
+            print(ptr());
+            std::cout << "pt = " << pt << std::endl;
+            std::cout << "key = " << pt->key_ << std::endl;
+            if ( is_disbalance( height_difference(pt->right_, pt->left_) ) ) {
+                std::cout << "IS DISBALANCE\n";
+                if (height_difference(pt->right_->right_, pt->right_->left_) >= 0) {
+                    std::cout << "LEFT TURN\n";
+            std::cout << "key = " << pt->key_ << std::endl;
+                    left_turn(pt);
+                } else {
+                    big_left_turn(pt);
+                }
+            } else if ( is_disbalance( height_difference(pt->left_, pt->right_) ) ) {
+                if (height_difference(pt->left_->left_, pt->left_->right_) >= 0) {
+                    right_turn(pt);
+                } else {
+                    big_right_turn(pt);
+                } 
             }
-        } else if ( is_disbalance( height_difference(pt->left, pt->right) ) ) {
-            if (height_difference(pt->left->left, pt->left->right) >= 0) {
-                right_turn(pt);
-            } else {
-                big_right_turn(pt);
-            } 
+            pt = pt->parent_;
         }
     }
 
@@ -147,6 +187,8 @@ private:
         Node(KeyT key = 0, pointer parent = nullptr)
         : key_ {key},
           parent_ {parent} {};
+
+        ~Node() = default;
     /*--------------------------------------------------------------*/
         KeyT key_;
         pointer parent_ , left_ {nullptr}, right_ {nullptr};
