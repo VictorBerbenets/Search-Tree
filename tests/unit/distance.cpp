bb6ffc5f5cb1bf2d0bedd2273919a1a70cd34319
Victor Berbenets <berbenets.vd@phystech.edu>
2023-10-13 00:38:44 +0300

    made tests for distant method

--- tests/unit/distance.cpp
+++ tests/unit/distance.cpp
@@ -1,6 +1,47 @@
 #include <gtest/gtest.h>
+#include <iterator>
 #include <set>
 
 #include "search_tree.hpp"
 
 using namespace yLAB;
+
+TEST(DistanceMethod, Distance1) {
+    AVL_Tree<int> avl{};
+    auto it1 = avl.begin();
+    auto it2 = avl.end();
+
+    ASSERT_EQ(avl.distance(it1, it2), std::distance(it1, it2));
+}
+
+TEST(DistanceMethod, Distance2) {
+    AVL_Tree<int> avl{1};
+    auto it1 = avl.begin();
+    auto it2 = avl.end();
+
+    ASSERT_EQ(avl.distance(it1, it2), std::distance(it1, it2));
+}
+
+TEST(DistanceMethod, Distance3) {
+    AVL_Tree<int> avl{1, 2, 3, 4, 5, -1, -2, -3, -4, -5, 0};
+    auto it1 = avl.begin();
+    auto it2 = avl.end();
+
+    ASSERT_EQ(avl.distance(it1, it2), std::distance(it1, it2));
+}
+
+TEST(DistanceMethod, Distance4) {
+    AVL_Tree<int> avl{};
+    constexpr int size = 100;
+    for (auto i = 0; i < size; ++i) {
+        avl.emplace(i);
+    }
+
+    for (int i = 0; i < size - 1; ++i) {
+        auto it1 = avl.find(i);
+        for (int j = i; j < size - 1; ++j) {
+            auto it2 = avl.find(j);
+            ASSERT_EQ(avl.distance(it1, it2), std::distance(it1, it2));
+        }
+    }
+}
